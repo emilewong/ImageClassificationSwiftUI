0fee43c00be56cd48fa30f1026f2723495ec3fdb
Emile Wong <emilewong@outlook.com>
2021-05-15 00:50:11 +0800

    Initial Commit

---
+++ ImageClassificationSwiftUI/ImageClassificationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageClassificationSwiftUIApp.swift
+//  ImageClassificationSwiftUI
+//
+//  Created by Emile Wong on 15/5/2021.
+//
+
+import SwiftUI
+
+@main
+struct ImageClassificationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
